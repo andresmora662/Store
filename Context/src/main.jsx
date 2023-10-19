80920f0d1d316b5f58f49c3386c286d761279e64
Andres Mora <123229413+andresmora662@users.noreply.github.com>
2023-10-19 18:52:59 -0500

    npm build

--- Context/src/main.jsx
+++ Context/src/main.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
-import './index.css'
+import '../src/styles/index.css'
 import {createBrowserRouter, RouterProvider} from 'react-router-dom'
 import Login from './components/Login.jsx'
 import { UserProvider } from './context/UserContext.jsx'
